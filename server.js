8610a9d28df51104942bf9f49e61f4b68b0cc8b0
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2015-01-24 00:25:16 -0700

    preparing app to be deployed on heroku

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var app = express();
 var http = require('http');
 var httpServer = http.Server(app);
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/snake.html');
 });
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
